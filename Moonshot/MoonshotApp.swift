dad162a9ba346e2df5f92d2c6f893d9465012fea
Sergey Petrosyan <ipetriez@icloud.com>
2024-03-03 20:08:51 +0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Sergey Petrosyan on 03.03.24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
