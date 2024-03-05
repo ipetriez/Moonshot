7b062b609d17a81a83dffbcd23c1415846137d94
Sergey Petrosyan <ipetriez@icloud.com>
2024-03-05 10:21:43 +0400

    Refactor Bundle.decode(_:), leveraging Generics in order to make this method work with any type that conforms to Decodable protocol.

--- Moonshot/Bundle-Decodable.swift
+++ Moonshot/Bundle-Decodable.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Bundle {
-    func decode(_ file: String) -> [String: Astronaut] {
+    func decode<T: Decodable>(_ file: String) -> T {
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in the Bundle.")
         }
@@ -17,7 +17,7 @@ extension Bundle {
             fatalError("Failed to load \(file) from the Bundle.")
         }
         
-        guard let loaded = try? JSONDecoder().decode([String: Astronaut].self, from: data) else {
+        guard let loaded = try? JSONDecoder().decode(T.self, from: data) else {
             fatalError("Failed to decode \(file) from the Bundle.")
         }
         
