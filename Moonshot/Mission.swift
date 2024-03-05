ca2e089f781e447b23996457d49d591551a3948f
Sergey Petrosyan <ipetriez@icloud.com>
2024-03-05 11:36:18 +0400

    Add logic for showing date in easy to read format.

--- Moonshot/Mission.swift
+++ Moonshot/Mission.swift
@@ -9,11 +9,12 @@ import Foundation
 
 struct Mission: Codable, Identifiable {
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
     var displayName: String { "Apollo \(id)" }
     var imageName: String { "apollo\(id)" }
+    var formattedLaunchDate: String { launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A" }
     
     struct CrewRole: Codable {
         let name: String
